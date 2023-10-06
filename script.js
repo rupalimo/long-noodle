0b7f4b11f30dd3fb26108012db06c11e342eb334
rupali <36826017+rupalimo@users.noreply.github.com>
2023-10-06 15:45:48 -0400

    finalize text and styling

--- script.js
+++ script.js
@@ -30,3 +30,54 @@ function checkMe(selected) {
 // 01 set text input toString() as var
 // 02 store new value of var in html <p> element under div.rsvp, local storage
 // 03 refresh text input to original
+
+const btnShowAddress = document.querySelector('.address');
+
+btnShowAddress.addEventListener('click', btnHideAddress);
+
+function btnHideAddress() {
+	if (btnShowAddress.textContent === 'to my place') {
+		// address.style.display = 'block';
+		btnShowAddress.textContent = '7470 Rue Lajeunesse Apt 201';
+		btnShowAddress.style.border = '2px inset rgb(169, 133, 88)';
+		btnShowAddress.style.backgroundColor = 'rgb(222, 208, 200)';
+	} else {
+		btnShowAddress.textContent = 'to my place';
+		// address.style.display = 'none';
+		btnShowAddress.style.border = '2px outset rgb(222, 208, 200)';
+		btnShowAddress.style.backgroundColor = 'rgb(169, 133, 88)';
+	}
+}
+
+const btnShowDate = document.querySelector('.date');
+
+btnShowDate.addEventListener('click', btnHideDate);
+
+function btnHideDate() {
+	if (btnShowDate.textContent === 'on october 15') {
+		btnShowDate.textContent = 'next sunday';
+		btnShowDate.style.border = '2px inset #17ca36';
+		btnShowDate.style.backgroundColor = '#7bff91';
+	} else {
+		btnShowDate.textContent = 'on october 15';
+		btnShowDate.style.border = '2px outset #7bff91';
+		btnShowDate.style.backgroundColor = '#17ca36';
+	}
+}
+
+const btnShowTime = document.querySelector('.time');
+
+btnShowTime.addEventListener('click', btnHideTime);
+
+function btnHideTime() {
+	if (btnShowTime.textContent === '6pm to late') {
+		btnShowTime.textContent = 'whenever u want';
+		btnShowTime.style.border = '2px inset #c365ea';
+		btnShowTime.style.backgroundColor = '#dab8e9';
+	} else {
+		btnShowTime.textContent = '6pm to late';
+
+		btnShowTime.style.border = '2px outset #dab8e9';
+		btnShowTime.style.backgroundColor = '#c365ea';
+	}
+}
